71c78c41e63c5adc1fa39a9ec441d5ad4ff61239
Mikhail <Mikhail@DESKTOP-MQUF3T2>
2021-06-03 01:30:44 +0300

    20210603_0130

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -15,12 +15,18 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a;
-        a = sum2(1,2);
+        int a, b ;
+        a = sum(1,2);
+        b = sum (a,1,2);
+        System.out.println("a="+a+",b="+b);
     }
 
-    private static int sum2(int a, int b) {
+    private static int sum(int a, int b) {
      return a+b;
+    }
+
+    private static int sum(int a, int b, int c) {
+        return a+b-c;
     }
     
 }
---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Mikhail
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            
+        int [] x = {1,2,3};
+        System.out.println(sum(x));
+        System.out.println(foreachsum(x));
+    }
+    //вычислить сумму элекментов массива
+    private static int sum (int ... x){
+        int s=0, i=0;
+        for (i = 0; i < x.length; i++) {
+        s+=x[i];
+        }
+        return s;
+    }
+    private static int foreachsum (int ... x){
+        int s=0;
+        for (int xi:x) s+=xi;  //xi - временное значение, аналог счётчика i
+        return s;
+    }
+    
+}
