6917af11f1ee4c8d125595b7528efdc520b06b10
Mikhail <Mikhail@DESKTOP-MQUF3T2>
2021-06-03 00:56:17 +0300

    commit

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Mikhail
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] m = {3,2,4,1,5};
+        Arrays.sort(m);
+        System.out.println(Arrays.toString(m));
+    }
+    
+}
---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Mikhail
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a;
+        a = sum2(1,2);
+    }
+
+    private static int sum2(int a, int b) {
+     return a+b;
+    }
+    
+}
